bf30d08486dc6749fa373685ff0b410cd53d22c4
Your Name <you@example.com>
2020-11-19 23:29:19 +0100

    Thu Nov 19 23:29:19 CET 2020

--- rpms/src/devOS/src/root/install.sh
+++ rpms/src/devOS/src/root/install.sh
@@ -1,12 +1,12 @@
 rsync -aAXv / --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} /mnt
 rm /mnt/etc/fstab
 bash /root/genfstab -U /mnt >> /mnt/etc/fstab
-exit 0
+echo "to do in chroot
 arch-chroot /mnt pacman -Sy --noconfirm linux
-arch-chroot /mnt grub-install /dev/sda
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt grub2-install /dev/sda
+arch-chroot /mnt grub2--mkconfig -o /boot/grub/grub.cfg
 arch-chroot /mnt /bin/bash /root/adduser
-
+"
 echo "Enter hostname:"
 read myhost
 
