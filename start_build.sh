a4dca2b33ea07f1536ae685a4946a5931473ae1d
Your Name <you@example.com>
2020-11-19 21:07:14 +0100

    rebase

---
+++ start_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+check=$(composer-cli compose status  | grep -c RUNNING)
+
+if [ $check -gt 0 ]; then
+echo "Build already running..."
+
+composer-cli compose status
+exit 1
+fi
+
+cd old_tomls
+composer-cli blueprints save devOS
+cd ..
+composer-cli blueprints push devOS.toml
+composer-cli compose start devOS live-iso
